edfce0a137210a12b4fae649f3465271f6ef2d82
cyberc0dex <nexusbinary0101@gmail.com>
2025-12-29 10:07:27 +0800

    Bug fixes

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js - Service worker for system-insights
 
-const CACHE_NAME = 'rc-prod-v1.1.0';
+const CACHE_NAME = 'rc-prod-v1.1.1';
 const urlsToCache = [
     './',
     './index.html',
